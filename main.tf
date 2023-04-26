ead2c6edffec2b6152955e5857e4f40036ab877d
kishore.ram <kishore.ram@gmail.com>
2023-04-26 16:56:46 +0530

    Commented in main.tf

--- main.tf
+++ main.tf
@@ -28,14 +28,14 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "kmbl_s3_kms" {
     }
   }
 }
-
+/*
 resource "aws_s3_bucket_acl" "kmbl_s3_acl_private" {
   #depends_on = [aws_s3_bucket_ownership_controls.example]
 
   bucket = aws_s3_bucket.kmbl_s3.id
   acl    = "private"
 }
-
+*/
 resource "aws_s3_bucket_public_access_block" "kmbl_s3_block_public" {
   bucket = aws_s3_bucket.kmbl_s3.id
 
